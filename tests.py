09a26bb3b6a9443e6fb7f1aa804b806fc6b5bfc8
Toxagin <toxagin@gmail.com>
2016-12-27 20:54:10 +0300

    develop some testcases for format_price function for the check

--- tests.py
+++ tests.py
@@ -1,1 +1,28 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestPriceFormatter(unittest.TestCase):
+
+    def test_space_separator(self):
+        self.assertEqual(format_price('123456789.01'), '123 456 789.01')
+
+    def test_zeros_after_decimal_point(self):
+        self.assertEqual(format_price('1.000'), '1')
+
+    def test_integer_input(self):
+    	self.assertEqual(format_price(123456), '123 456')
+
+    def test_float_input(self):
+    	self.assertEqual(format_price(1234.123), '1 234.12')
+
+    def test_invalid_number(self):
+        with self.assertRaises(ValueError):
+            format_price('ewr23')
+
+    def test_invalid_type(self):
+    	with self.assertRaises(ValueError):
+    		format_price([1, 2])
+
+if __name__ == '__main__':
+    unittest.main()
