0e1412036f49812982660abcddcc90543e14cf8d
Toxagin <toxagin@gmail.com>
2016-12-27 20:52:50 +0300

    Implemented function format_price and creates commandline interface fot it

--- format_price.py
+++ format_price.py
@@ -1,5 +1,27 @@
+import re
+import argparse
+
+PATTERN_FLOAT_NUMBERS = r'^\d+\.?\d*$'
+
 def format_price(price):
-    pass
+    str_price = str(price)
+    if not re.search(PATTERN_FLOAT_NUMBERS, str_price):
+        raise ValueError
+    float_price = float(str_price)
+    if float_price.is_integer():
+        formatted_price = '{:,.0f}'.format(float_price).replace(',',' ')
+    else:
+        formatted_price = '{:,.2f}'.format(float_price).replace(',',' ')
+    return formatted_price    
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+    	description='Convert price into clear view'
+    )
+    parser.add_argument(
+    	'input',
+    	help='price that converts in readable format'
+    )
+    args = parser.parse_args()
+    print(format_price(args.input))
